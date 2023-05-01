02a5fee8e61f2832ce401c200472cebea9b4eaf2
David R <114369357+MuertoGB@users.noreply.github.com>
2023-05-01 02:34:44 +0100

    Update NativeMethods.cs

--- MacEfiTool/Interop/NativeMethods.cs
+++ MacEfiTool/Interop/NativeMethods.cs
@@ -6,21 +6,20 @@ using System.Runtime.Versioning;
 
 namespace Mac_EFI_Toolkit.Interop
 {
-    class nativemethods
+    class NativeMethods
     {
-
-        // https://www.pinvoke.net/default.aspx/user32.ReleaseCapture
+        // https://learn.microsoft.com/en-us/windows/win32/api/winuser/nf-winuser-releasecapture
         [DllImport("user32.dll", CharSet = CharSet.Auto, ExactSpelling = true)]
         [ResourceExposure(ResourceScope.None)]
         internal static extern bool ReleaseCapture(HandleRef hWnd);
 
-        // https://www.pinvoke.net/default.aspx/user32.SendMessage
+        // https://learn.microsoft.com/en-us/windows/win32/api/winuser/nf-winuser-sendmessage
         [DllImport("user32.dll", CharSet = CharSet.Auto)]
         [ResourceExposure(ResourceScope.None)]
         internal static extern IntPtr SendMessage(HandleRef hWnd, int Msg, IntPtr wParam, IntPtr lParam);
 
+        // https://learn.microsoft.com/en-us/windows/win32/api/wingdi/nf-wingdi-addfontmemresourceex
         [DllImport("gdi32.dll", ExactSpelling = true)]
         internal static extern IntPtr AddFontMemResourceEx(IntPtr pbFont, uint cbFont, IntPtr Pvd, [In] ref uint pcFonts);
-
     }
 }
