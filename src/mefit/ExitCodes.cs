b38f6cf9e5dfdfc3a61629e9ea39d65d3d7c01a1
David Roberts <qbug@tuta.io>
2025-05-27 12:26:53 +0100

    Update ExitCodes.cs

--- src/mefit/ExitCodes.cs
+++ src/mefit/ExitCodes.cs
@@ -2,9 +2,10 @@
 {
     internal class ExitCodes
     {
-            public const byte Success = 0;
-            public const byte ExceptionManager = 1;
-            public const byte FontImport = 2;
-            public const byte Generic = 99;
+        public const sbyte Unknown = -1;
+        public const byte Success = 0;
+        public const byte ExceptionManager = 1;
+        public const byte FontImport = 2;
+        public const byte Generic = 255;
     }
 }
